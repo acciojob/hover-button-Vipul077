73667c253b9447e079b089d47b699f466f144ec1
Vipul077 <134216118+Vipul077@users.noreply.github.com>
2023-09-09 12:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+// Select the button
+var btn = document.getElementById('btn');
+
+// Add event listener
+btn.addEventListener('click', function() {
+    // Code to execute when button is clicked
+    console.log('Button clicked!');
+});
